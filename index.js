1fb62d236f1e1d1a5df266cb29a783cd183c3998
Oumar <baarryoumar@gmail.com>
2022-12-31 22:22:54 -0500

    game done

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumb
 document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 +".png");
 
 if (randomNumber1 > randomNumber2){
-  document.querySelector("h1").textContent = "Player 1 wins!";
+  document.querySelector("h1").textContent = "🚩Player 1 wins!";
 }
 
 else if (randomNumber1 < randomNumber2){
-  document.querySelector("h1").textContent = "Player 2 wins!";
+  document.querySelector("h1").textContent = "Player 2 wins!🚩";
 }
 
 else{
